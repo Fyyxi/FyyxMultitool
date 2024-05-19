142c5dfcb9134a8d7dd0f0c62e0196613227a345
Fyyx <miconbisnes@gmail.com>
2024-05-19 22:59:01 +0300

    Update install_dependencies.bat

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -16,31 +16,51 @@ if %errorlevel% neq 0 (
 )
 
 :: Install required packages using Chocolatey
+echo Installing Nmap...
 choco install nmap -y
+if %errorlevel% neq 0 (
+    echo Error: Failed to install Nmap.
+    pause
+    exit /b 1
+)
+
+echo Installing PowerShell...
 choco install powershell -y
-
-:: Download speedtest CLI executable
-powershell -Command "Invoke-WebRequest -Uri 'https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-win64.zip' -OutFile 'speedtest.zip'"
 if %errorlevel% neq 0 (
-    echo Error: Failed to download speedtest utility.
+    echo Error: Failed to install PowerShell.
     pause
     exit /b 1
 )
 
-:: Extract speedtest CLI executable
-powershell -Command "Expand-Archive -Path 'speedtest.zip' -DestinationPath '.'"
-if %errorlevel% neq 0 (
-    echo Error: Failed to extract speedtest utility.
-    pause
-    exit /b 1
-)
+:: Check if speedtest CLI is already downloaded
+if not exist "speedtest.exe" (
+    echo Downloading speedtest CLI executable...
+    powershell -Command "Invoke-WebRequest -Uri 'https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-win64.zip' -OutFile 'speedtest.zip'"
+    if %errorlevel% neq 0 (
+        echo Error: Failed to download speedtest utility.
+        pause
+        exit /b 1
+    )
 
-:: Move speedtest executable to current directory
-move "ookla-speedtest-1.1.1-win64/speedtest.exe" .
-if %errorlevel% neq 0 (
-    echo Error: Failed to move speedtest utility.
-    pause
-    exit /b 1
+    echo Extracting speedtest CLI executable...
+    powershell -Command "Expand-Archive -Path 'speedtest.zip' -DestinationPath '.'"
+    if %errorlevel% neq 0 (
+        echo Error: Failed to extract speedtest utility.
+        pause
+        exit /b 1
+    )
+
+    echo Moving speedtest executable to current directory...
+    move "ookla-speedtest-1.1.1-win64/speedtest.exe" .
+    if %errorlevel% neq 0 (
+        echo Error: Failed to move speedtest utility.
+        pause
+        exit /b 1
+    )
+
+    echo Cleaning up downloaded files...
+    del speedtest.zip
+    rd /s /q "ookla-speedtest-1.1.1-win64"
 )
 
 echo Libraries installed successfully.
